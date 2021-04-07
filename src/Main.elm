f0020c6b43c038f75174dc6de1a9cdbd1e1c15ec
mahidul-islam <mizihan84@gmail.com>
2021-04-07 21:58:47 +0600

    added ball and box to the elm playground

--- src/Main.elm
+++ src/Main.elm
@@ -1,54 +1,94 @@
-module Main exposing (..)
+module Main exposing (main, update, view)
 
-import Browser
-import Html exposing (Html)
-import Html.Attributes exposing (src)
+import Playground exposing (..)
 
 
-
----- MODEL ----
-
-
-type alias Model =
-    {}
+type alias Memory =
+    { ball : Ball
+    , box : Box
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+type alias Box =
+    { width : Float
+    , height : Float
+    }
 
 
-
----- UPDATE ----
-
-
-type Msg
-    = NoOp
+type alias Ball =
+    { x : Float, y : Float, speed : Float, length : Float }
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
+box : Box
+box =
+    { width = 960, height = 600 }
 
 
-
----- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    Html.text "Hello"
+ball : Ball
+ball =
+    { x = 0, y = 0, speed = 8, length = 20 }
 
 
+memory : Memory
+memory =
+    { ball = ball
+    , box = box
+    }
 
----- PROGRAM ----
 
-
-main : Program () Model Msg
+main : Program () (Game Memory) Msg
 main =
-    Browser.element
-        { view = view
-        , init = \_ -> init
-        , update = update
-        , subscriptions = always Sub.none
+    game view update memory
+
+
+view : Computer -> Memory -> List Shape
+view computer memory1 =
+    [ drawBall memory1.ball
+    , drawBox memory1.box
+    ]
+
+
+drawBall : Ball -> Shape
+drawBall ball4 =
+    rectangle blue ball4.length ball4.length
+        |> move ball4.x ball4.y
+
+
+drawBox : Box -> Shape
+drawBox box2 =
+    [ rectangle red 1 box2.height
+        |> move
+            (box2.width
+                / 2
+            )
+            0
+    , rectangle red 1 box2.height
+        |> move
+            -(box2.width
+                / 2
+             )
+            0
+    , rectangle red box2.width 1
+        |> move 0
+            (box2.height
+                / 2
+            )
+    , rectangle red box2.width 1
+        |> move 0
+            -(box2.height
+                / 2
+             )
+    ]
+        |> group
+
+
+update : Computer -> Memory -> Memory
+update computer memory2 =
+    { ball =
+        { x = memory2.ball.x + (toX computer.keyboard * memory2.ball.speed)
+        , y = memory2.ball.y + (toY computer.keyboard * memory2.ball.speed)
+        , speed = memory2.ball.speed
+        , length = memory2.ball.length
         }
+    , box = memory2.box
+    }
