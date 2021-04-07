3786e298908d9c23ec4dbc660b8d3b213843a8f0
mahidul-islam <mizihan84@gmail.com>
2021-04-07 17:36:54 +0600

    initial commit

---
+++ src/Main.elm
@@ -0,0 +1,54 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html)
+import Html.Attributes exposing (src)
+
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    Html.text "Hello"
+
+
+
+---- PROGRAM ----
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
