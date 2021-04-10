dfc6e14594af549d9c10fd17f22e0001dc4380fe
mahidul-islam <mizihan84@gmail.com>
2021-04-11 00:23:45 +0600

    Updates

---
+++ precache-manifest.20bcb81bea222e1efb54849b29d99863.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/pong_new/favicon.ico"
+  },
+  {
+    "revision": "a8f4f8cad43e6492f4496fb8beeb927c",
+    "url": "/pong_new/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/pong_new/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/pong_new/manifest.json"
+  },
+  {
+    "revision": "df38e01f6416b2c0a040",
+    "url": "/pong_new/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "df38e01f6416b2c0a040",
+    "url": "/pong_new/static/js/main.df38e01f.chunk.js"
+  },
+  {
+    "revision": "814b7cee1581bb8a34ce",
+    "url": "/pong_new/static/js/runtime~main.814b7cee.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/pong_new/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
